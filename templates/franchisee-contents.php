ab460f0a4951b860857b2b71fc7644c2588358d0
nefetina <nefetinaira.tomas@gsfe.tupcavite.edu.ph>
2023-03-23 14:55:17 +0800

    fix

--- templates/franchisee-contents.php
+++ templates/franchisee-contents.php
@@ -61,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' && realpath(__FILE__) == realpath($_SERV
                                             <span class="fas fa-building"></span>
                                         </div>
                                     </div>
-                                    <input type="text" class="form-control" placeholder="Company" name="company" autocomplete="off">
+                                    <input type="text" class="form-control" placeholder="Company" name="company" onkeypress="return event.keyCode !== 39 && event.keyCode !== 34;" autocomplete="off">
                                     <div class="input-group-append" style="padding-left: 10px;">
                                         <div class="input-group-text">
                                             <span class="fas fa-map-marker-alt"></span>
