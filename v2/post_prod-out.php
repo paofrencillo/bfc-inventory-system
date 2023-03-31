fc788342534434046228670469a821fbc859ad2a
paopaolopaoloo <simonruzzel@gmail.com>
2023-03-31 09:18:28 +0800

    change root directory

---
+++ v2/post_prod-out.php
@@ -0,0 +1,48 @@
+<?php
+include('templates/connection.php');
+
+
+
+// Get the values from the AJAX request
+$barcode = $_POST['barcode'];
+$description = $_POST['description'];
+$quantity = $_POST['quantity'];
+$lot = $_POST['lot'];
+$exp_date = $_POST['exp_date'];
+$endorsed_date = $_POST['endorsed_date'];
+$remarks = $_POST['remarks'];
+$endorsed_by = $_POST['endorsed_by'];
+$mrf = $_POST['mrff'];
+$order_num = $_POST['order_numm'];
+$branch = $_POST['branch'];
+$branch22 = $_POST['branch22'];
+
+// Insert the values into the database
+$sql = "INSERT INTO endorse (
+    barcode, 
+    description, 
+    quantity, 
+    lot,
+    branch, 
+    mrf,
+    order_num, 
+    exp_date,
+    remarks, 
+    endorsed_by,
+    endorsed_date) 
+    VALUES (
+    '$barcode', 
+    '$description',
+    '$quantity', 
+    '$lot',
+    '$branch', 
+    '$mrf',
+    '$order_num', 
+    '$exp_date',
+    '$remarks', 
+    '$endorsed_by', 
+    '$endorsed_date')";
+mysqli_query($conn, $sql);
+
+
+mysqli_close($conn);
---
+++ v2/search_prod-out.php
@@ -0,0 +1,29 @@
+<?php
+date_default_timezone_set('Asia/Manila');
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "bfc_inventory";
+
+$conn = mysqli_connect($server, $user, $pass, $db);
+
+if (!$conn) {
+  die("<script>alert('Connection Failed.')</script>");
+}
+
+header("Content-Type: text/json; charset=utf8");
+
+$mrf_search = $_POST['mrf_search'];
+$endorsed_by = $_POST['endorsed_by'];
+
+// Query your MySQL database and retrieve the data
+$query = "SELECT * FROM endorse_final WHERE mrf = '$mrf_search' and endorsed_by = '$endorsed_by' ORDER BY description";
+$result = $conn->query($query);
+// Convert result set to JSON format
+$rows = array();
+while ($row = $result->fetch_assoc()) {
+  $rows[] = $row;
+}
+echo json_encode($rows);
+?>
